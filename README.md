246f0428e056c886aff2e1cf3c2dbd2ae406b874
mg <43611161+hx41455@users.noreply.github.com>
2021-02-13 10:52:05 -0500

    Changed the formatting of how the url is taken in, added hotbar functionality

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # BLU Hud layout macro generator
 Because I'm too lazy to craft macros, but not too lazy to write a python script that will craft them for me...
 
-Use https://xivbars.bejezus.com/job/BLU to generate a few HUDs, then get the share url (click the copy button) to get that, and run `blu.py xivbars.bejezus.com/job/BLU?s=[["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"]]` to generate macros (where the zeros will be spell IDs)
+Use https://xivbars.bejezus.com/job/BLU to generate a few HUDs, then get the share url (click the copy button) to get that, and run `generateActions.py 'xivbars.bejezus.com/job/BLU?s=[["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"]]&l=1'` to generate macros (where the zeros will be spell IDs)
 
 XIVbars doesn't have Phantom Flurry or Nightbloom yet, so you'll either have to add those macros manually, or find their spots in the url and add their IDs (23288 and 23290 respectively)
 
--- generateMacros.py
+++ generateMacros.py
@@ -17,19 +17,16 @@ from datetime import datetime
 
 # SAMPLE = "xivbars.bejezus.com/job/BLU?s=[["11383","0","0","0","11384","0","0","0","11385","0","0","0","11386","0","0","0"],["11392","0","0","0","11393","0","0","0","11394","0","0","0","11389","0","0","0"],["11399","0","0","0","11406","0","0","0","11416","0","0","0","11412","0","0","0"],["11427","0","0","0","11430","0","0","0","11424","0","0","0","11415","0","0","0"]]"
 
-async def genMacro(argv):
+def genMacro(argv):
     osm = platform.system()
     layout = []
     huds = []
     hudmacro = []
     bookmacro = []
 
-    if osm == "Windows":
-        layout.append(argv[0].split("=[[")[1])
-        for i in range(1,4):
-            layout.append(argv[i].split("],[")[1])
-    else:
-        layout = argv[0].split("=")[1].split("[[")[1].split("]]")[0].split("],[")
+    # 0 for xbar; 1 for hotbar
+    hbar = int(argv[0].split("&")[1].split("=")[1])
+    layout = argv[0].split("=")[1].split("[[")[1].split("]]")[0].split("],[")
 
     for i in range(0, len(layout)):
         huds.append(layout[i].split(","))
@@ -51,19 +48,25 @@ async def genMacro(argv):
                         bookmacro.append("/bluespellbook set \"" + actions[index_name][id] + "\"")
                     
                     action = actions[index_name][id]
-                    hudmacro.append("/chotbar " + index_name + " \"" + action + "\" " + str(i+1) + " " + ("L" if j < 8 else "R") + ("D" if (j%8) < 4 else "A") + str(((j-1)%4)+1))
+                    if not hbar:
+                        hudmacro.append("/chotbar " + index_name + " \"" + action + "\" " + str(i+1) + " " + ("L" if j < 8 else "R") + ("D" if (j%8) < 4 else "A") + str(((j-1)%4)+1))
+                    elif hbar:
+                        hudmacro.append("/hotbar " + index_name + " \"" + action + "\" " + str(i+1) + " " + str(j+1))
 
+    print('\n')
     with open(datetime.now().strftime("%Y%m%d%H%M%S")+"_hud.txt",'w') as file:
         for h in hudmacro:
+            print(h)
             file.write('%s\n' % h)
+    print('\n')
     with open(datetime.now().strftime("%Y%m%d%H%M%S")+"_book.txt",'w') as file:
         for b in bookmacro:
+            print(b)
             file.write('%s\n' % b)
+    print('\n')
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("Use by passing the sharable url `https://xivbars.bejezus.com/?job=BLU`")
+        print("Use by passing the sharable url `https://xivbars.bejezus.com/?job=BLU`\nMust pass inside single quotes '[content]'")
     else:
-        # logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='%H:%M')
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(genMacro(sys.argv[1:]))
+        genMacro(sys.argv[1:])
